91e97e82e3c5dc434039d89b3f25298483813432
Marcel Ritter <marcel.ritter@uibk.ac.at>
2024-07-15 13:57:52 +0200

    script to prep bin files, updated first version binaries

---
+++ src/prepBin.bat
@@ -0,0 +1,38 @@
+echo %1
+cd bin\Debug
+
+powershell rm -r -fo .\%1\
+
+mkdir %1
+
+xcopy *.dll %1
+xcopy Accord.dll.config %1
+xcopy ExperimentSetup.conf %1
+xcopy EyeTargets.conf %1
+xcopy InputData.conf %1
+xcopy SPBView.exe %1
+xcopy SPBView.exe.config %1
+
+powershell Compress-Archive -Path %1 %1_Debug.zip
+
+move %1_Debug.zip ..\..\..\bin
+
+cd ..
+cd Release
+
+powershell rm -r -fo .\%1\
+
+mkdir %1
+
+xcopy *.dll %1
+xcopy Accord.dll.config %1
+xcopy ExperimentSetup.conf %1
+xcopy EyeTargets.conf %1
+xcopy InputData.conf %1
+xcopy SPBView.exe %1
+xcopy SPBView.exe.config %1
+
+powershell Compress-Archive -Path %1 %1_Release.zip
+
+move %1_Release.zip ..\..\..\bin
+
